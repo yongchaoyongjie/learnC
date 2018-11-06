662817c2989350e5bd7a64b182149ae32b50049f
yongchaoyongjie <215743260@qq.com>
2018-11-06 17:10:50 +0800

    10.27

---
+++ 10.27/10.27.c/yuan.c
@@ -0,0 +1,19 @@
+# define _CRT_SECURE_NO_WARNINGS
+# include<stdio.h>
+# include<stdlib.h>
+void test(){
+	int a = 0;
+	a = a + 1;
+	printf("a=%d\n", a);
+	return ;
+}
+int mian(){
+	int i = 0;
+	for (i = 1; i < 10; i++); {
+		void test£¨£©;
+	}
+	printf("i=%d\n", i);
+			system("pause");
+		return 0;
+	
+}
